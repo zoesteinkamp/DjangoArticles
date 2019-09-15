28c1a7605610b4bc851c10c28807bc644c3ae2a5
Zoesteinkamp <zoe.steinkamp@gmail.com>
2019-09-14 21:47:39 -0600

    The quotes now only appear if relevant

--- mysite/articles/views.py
+++ mysite/articles/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.core import serializers
 from .forms import CommentForm
-from .models import Article, Quote, Comment, Author
+from .models import Article, Quote, Comment, Author, Instrument
 import itertools
 import json
+from django.db.models import Q
+
 
 def home(request):
     qs1 = Article.objects.filter(tags__slug ="10-promise")[:1]
@@ -19,6 +21,9 @@ def article(request, article_uuid):
     article = get_object_or_404(Article, pk=article_uuid)
     comments = article.comments.order_by('-created')[:10] # set a reasonable amount and have the newest at the top
     suggested_articles = Article.objects.all()[:3]
+    insturments = article.insturments.all()
+    symbolset = set(instrument.symbol for instrument in insturments)
+    companyset = set(instrument.company_name for instrument in insturments)
     stock_list = json.dumps([{
         'company_name': quote.company_name,
         'symbol': quote.symbol,
@@ -26,7 +31,8 @@ def article(request, article_uuid):
         'currentpriceamount': quote.currentpriceamount,
         'changeamount': quote.change_amount,
         'changepercent': quote.percent_change
-    } for quote in Quote.objects.all()])
+    } for quote in Quote.objects.filter(Q(symbol__in= symbolset) | Q(company_name__in= companyset)) ])
+    # the code above will grab only the relevant Stocks to this article, with the symbol
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
